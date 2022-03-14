c6eed168bb5a49e8b06c3dde442a7996cd7c0141
Ryan Zimmerman <zimmerry@oregonstate.edu>
2022-03-14 14:26:34 -0700

    Add Damage Type and Magic School activities

--- app/src/main/java/com/example/infinidnd/MainActivity.kt
+++ app/src/main/java/com/example/infinidnd/MainActivity.kt
@@ -1,11 +1,25 @@
 package com.example.infinidnd
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val damageBtn: Button = findViewById(R.id.button_damage)
+        damageBtn.setOnClickListener {
+            startActivity(Intent(this, DamageActivity::class.java))
+        }
+
+        val schoolsBtn: Button = findViewById(R.id.button_schools)
+        schoolsBtn.setOnClickListener {
+            startActivity(Intent(this, SchoolsActivity::class.java))
+        }
+
     }
 }
